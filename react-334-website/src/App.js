ba4853a296c1e7f99f30102f1d97ff418ba1cb9b
nguyenminh2440 <nguyenminh2440@gmail.com>
2022-04-28 09:52:12 +0700

    App setup

--- react-334-website/src/App.js
+++ react-334-website/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
+    <>
+    <Router>
     <div className="App">
       <h1>
         334 Prescription
       </h1>
+      <Sidebar></Sidebar>
+      <Routes>
+        <Route path ='/' exact />
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
